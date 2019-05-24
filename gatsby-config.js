8b1713d4170475b888f618b870c99a840040c300
Lukas Marx <marxlukas@googlemail.com>
2019-05-24 15:25:56 +0200

    feat(analytics): added google analytics feat(privacy): updated privacy policy feat(post): angular async pipe

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,27 @@ module.exports = {
     siteUrl: `https://malcoded.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['UA-87372848-1'],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared accross all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+          allow_ad_personalization_signals: false,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
     `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
