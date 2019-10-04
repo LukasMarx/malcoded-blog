db033738fdce25e82f46e4357c5b6e751a566c3b
Lukas <marxlukas@googlemail.com>
2019-10-04 16:15:33 +0000

    feat(analytics): enabled custom analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,27 +3,37 @@ import io from 'socket.io-client'
 import { wrapRootElement as wrap } from './wrap-root-element'
 
 export const wrapRootElement = wrap
-
 let socket
+let userLocation
+
+export const onClientEntry = () => {
+  var myRequest = new XMLHttpRequest()
+
+  myRequest.onload = function(e) {
+    userLocation = JSON.parse(myRequest.response).location
+  }
+  myRequest.open('GET', 'https://malcoded.com/v1/api/geoip/isEu', true)
+  myRequest.send()
+}
 
 export const onRouteUpdate = ({ location, prevLocation }) => {
-  // if (!socket) {
-  //   console.log('creating socket')
-  //   socket = io('http://localhost:54247', {
-  //     path: '/v1/api/ws',
-  //     // transports: ['websocket'],
-  //   })
-  //   socket.on('connect', () => {
-  //     console.log('Success')
-  //   })
-  //   socket.on('error', err => {
-  //     console.error(err)
-  //   })
-  //   socket.open()
-  // }
-  // socket.emit('event', {
-  //   type: 'pageview',
-  //   pageLocation: location.pathname,
-  //   userLocation: 'DE',
-  // })
+  if (!socket) {
+    console.log('creating socket')
+    socket = io('https://malcoded.com', {
+      path: '/v1/api/ws',
+      // transports: ['websocket'],
+    })
+    socket.on('connect', () => {
+      console.log('Success')
+    })
+    socket.on('error', err => {
+      console.error(err)
+    })
+    socket.open()
+  }
+  socket.emit('event', {
+    type: 'pageview',
+    pageLocation: location.pathname,
+    userLocation: userLocation,
+  })
 }
