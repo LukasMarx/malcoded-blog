5da27be6653cc74b6e9c4eb949ebfa51a290c0d5
Lukas <marxlukas@googlemail.com>
2019-10-05 15:44:05 +0000

    fix(ws): terminating websocket connection after 5 minutes of hidden tab

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,35 @@ import { wrapRootElement as wrap } from './wrap-root-element'
 
 export const wrapRootElement = wrap
 let socket
+let hiddenTimer
+
+var hidden, visibilityChange
+if (typeof document.hidden !== 'undefined') {
+  // Opera 12.10 and Firefox 18 and later support
+  hidden = 'hidden'
+  visibilityChange = 'visibilitychange'
+} else if (typeof document.msHidden !== 'undefined') {
+  hidden = 'msHidden'
+  visibilityChange = 'msvisibilitychange'
+} else if (typeof document.webkitHidden !== 'undefined') {
+  hidden = 'webkitHidden'
+  visibilityChange = 'webkitvisibilitychange'
+}
+
+document.addEventListener(visibilityChange, e => {
+  if (document[hidden]) {
+    hiddenTimer = setTimeout(() => {
+      if (socket) {
+        socket.close()
+      }
+    }, 300000)
+  } else {
+    if (hiddenTimer) {
+      clearTimeout(hiddenTimer)
+      hiddenTimer = undefined
+    }
+  }
+})
 
 export const onRouteUpdate = ({ location, prevLocation }) => {
   if (!socket) {
